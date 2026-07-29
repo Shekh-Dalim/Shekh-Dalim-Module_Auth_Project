1412524a0abc0959baef8f9f5f83c78a1a107d90
Dalim <dalimshekh314@gmail.com>
2026-07-29 18:13:04 +0530

    Module_Auth_app

---
+++ src/main/java/com/Dalim_Auth_App/Dalim_Project_Backend/DalimProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.Dalim_Auth_App.Dalim_Project_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DalimProjectBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DalimProjectBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Dalim_Auth_App/Dalim_Project_Backend/DalimProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Dalim_Auth_App.Dalim_Project_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DalimProjectBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
